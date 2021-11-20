fd08d288f6b6c4612562e96e530509feb2677968
ded-git <sakarailko@gmail.com>
2021-11-20 18:10:25 +0200

    slider do it

--- js/app.js
+++ js/app.js
@@ -2,20 +2,17 @@ $(".slider__for").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
-    asNavFor: '.slider__nav',
     prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/prev.svg" alt=""></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="../images/next.svg" alt=""></button>',
 });
 
-// $('.slick-arrow').on('click', function(){
-//     $('.slick-arrow').css('transform', 'scale(0.8)');
-// });
+
 
 $('.slider__nav').slick({
-    slidesToShow: all,
+    slidesToShow: 6,
     slidesToScroll: 1,
     asNavFor: '.slider__for',
-    focusOnSelect: true,
-//     centerPadding: 0,
     infinite: true,
+    focusOnSelect: true,
+    variableWidth: true
   });
